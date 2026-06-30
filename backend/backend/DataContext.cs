b452ca2635bac02f5c88e9115396db782a48c09d
Jakob Faschang <jakob.faschang@schule.at>
2026-06-30 09:01:31 +0200

    DataContext created

---
+++ backend/backend/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace backend;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    // Das wird deine einzige Tabelle in der SQLite-Datenbank
+    public DbSet<PasswordEntry> Passwords { get; set; }
+}
