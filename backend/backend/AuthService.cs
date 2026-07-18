083f9b0e171d84f245df8e40cc6f22ff5b900927
Jakob Faschang <jakob.faschang@schule.at>
2026-07-18 20:01:44 +0200

    Feat: Created AuthService for PasswordHashing

---
+++ backend/backend/AuthService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace backend;
+
+public class AuthService
+{
+    private readonly PasswordHasher<Users> _hasher = new();
+
+    public string HashPassword(Users user, string password) 
+        => _hasher.HashPassword(user, password);
+
+    public bool VerifyPassword(Users user, string hashedPassword, string providedPassword)
+        => _hasher.VerifyHashedPassword(user, hashedPassword, providedPassword) == PasswordVerificationResult.Success;
+}
